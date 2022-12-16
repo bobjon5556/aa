cb3870bc7f8d49784472ca72ff831a0bdd3538b8
aaa4576253 <whanswkdz@gmail.com>
2022-12-16 14:22:52 +0900

    first commit

---
+++ whatisprogram/whatisprogram/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace whatisprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            /*
+             * 문자열 보간법
+             * 문자열보간법 또는 문자열 탬플릿이라고도 한다.
+             * 문자열을 묶어서 처리하기 위한 기능이다. 기존에는 String() aptjemfmf wnfh tkdydgoTdjTsmsep,
+             * C#6.0버전부터 $"{}" 형태로 간결하게 제공하고 있다.
+             * 
+             */
+
+            string hello = "Hello";
+            string world = "world";
+            Console.WriteLine("{0},{1}", hello, world);
+            Console.WriteLine($"{hello}, {world}");
+
+            const int THREE_NUMBER = 3;
+            const string ODD_WORD = "홀수";
+            Console.WiteLine($"{HREE_NUMBER}
+
+
+        }
+    }
+}
