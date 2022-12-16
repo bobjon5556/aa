544ba4343fcceafd9025cc811464c0d9627ac26d
aaa4576253 <whanswkdz@gmail.com>
2022-12-16 17:46:35 +0900

    first commit

---
+++ test2/test2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            {
+                Console.Write("원의 반지름 : ");
+                int i = int.Parse(Console.ReadLine());
+                double d = i * 3.14;
+                Console.WriteLine(i + 1 * Math.PI * d*d );
+
+            }
+        }
+    }
+}
---
+++ whatisprogram/whatisprogram/Class1.cs
@@ -0,0 +1,8 @@
+﻿static void main(string[] args)
+{
+    Console.Write("원의 반지름 : ");
+    int i = int.Parse(Console.ReadLine());
+    double d = i * 3.14;
+    Console.WriteLine(i + "둘레 =" + 2 * Math.PI * d * d + "넓이");
+
+}
