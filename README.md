f685e575c4c32f540572a73d8d935c8cdbfb743a
bobjon5556 <whanswkdz@gmail.com>
2022-12-21 05:02:21 +0900

    aa

---
+++ README.md
@@ -0,0 +1,9 @@
+# post
+
+처음 셋팅 후 두번째 부터는 아래 코드를 순서대로 한줄씩 입력합니다.
+
+git add * 
+
+git commit -m "커밋명"
+
+git push
---
+++ test2/test2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            {
+                Console.Write("원의 반지름 : ");
+                int i = int.Parse(Console.ReadLine());
+                double d = i;
+                Console.WriteLine("겉넓이=" + 4 * Math.PI * i*i + ", 부피="+4/3 * Math.PI * i*i*i);
+
+            }
+        }
+    }
+}
