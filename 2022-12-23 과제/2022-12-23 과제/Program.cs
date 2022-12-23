966bedd4724c74d0e7e64e3d2598a73f6328a1a4
bobjon5556 <whanswkdz@gmail.com>
2022-12-23 17:39:19 +0900

    main

---
+++ 2022-12-23 과제/2022-12-23 과제/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace _2022_12_23_과제
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Hello, World!");
+
+                //1.사용자로 부터 2개의 문자열을 읽어서 비교
+
+                Console.WriteLine("첫번째 문자를 입력하세요");
+                string input1 = Console.ReadLine();
+
+                Console.WriteLine("두번째 문자를 입력하세요");
+                string input2 = Console.ReadLine();
+
+
+                if (input1 != input2)
+                { Console.WriteLine("일치 하지 않습니다."); }
+
+                if (input1 == input2)
+                { Console.WriteLine("동일한 문자입니다."); }
+
+            }
+
+            //2.5개의 음료를 판매하는 자판기 머신을 구현해보기
+
+            Console.WriteLine("좋아하시는 음료와 번호를 선택하세요.");
+           
+            Console.WriteLine("1번:콜라,2번:물,3번:스프라이트, 4번:주스, 5번:커피");
+            
+            string input3 = Console.ReadLine();
+            int choice = Convert.ToInt32(input3);
+
+            for (; ; )
+            {
+                if (choice <= 5)
+                {
+                    switch (input3)
+                    {
+                        case "1":
+                            Console.WriteLine("콜라입니다");
+                            break;
+                        case "2":
+                            Console.WriteLine("물입니다");
+                            break;
+                        case "3":
+                            Console.WriteLine("스프라이트입니다");
+                            break;
+                        case "4":
+                            Console.WriteLine("주스입니다");
+                            break;
+                        case "5":
+                            Console.WriteLine("커피입니다");
+                            break;
+                    }
+
+                }
+                else
+                { Console.WriteLine("잘못된 입력입니다"); }
+
+                break;
+            }
+
+            //배열 days[]를 다음과 같이 초기화 하고 배열요소의 값을 다음과 같이 출력하는 프로그램 작성
+
+            
+            int[] days = new int[12] { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            int[] month1 = new int[12] {1,2,3,4,5,6,7,8,9,10,11,12 };
+
+            for (int i=0;i<13 ;i++ )
+            {
+
+                Console.WriteLine("{0}월은"+"{1}까지입니다.",month1[i], days[i]);
+
+                
+            }
+            
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ 모험가 이야기1/모험가 이야기1/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace 모험가_이야기1
+{
+
+
+
+    
+
+    internal class Program
+    {
+
+
+        class open
+        { }
+        class close
+        { }
+
+
+        class tron
+        { }
+
+
+
+            static void Main(string[] args)
+            {
+
+                int 체력 = 100;
+                int 방어력 = 20;
+                int 공격력 = 20;
+
+                int 적체력 = 50;
+                int 적방어력 = 10;
+                int 적공격력 = 10;
+
+                int 선택 = 0;
+                int 임시 = 0;
+
+
+                Console.WriteLine("Hello, World!");
+                Console.ReadKey();
+
+                {
+                    Console.WriteLine("직업을 선택하세요");
+                    Console.WriteLine("[1] 기사");
+                    Console.WriteLine("[2] 궁수");
+                    Console.WriteLine("[3] 전사");
+
+                    string input1 = Console.ReadLine();
+                    int choice = Convert.ToInt32(input1);
+
+                    for (; ; )
+                    {
+                        if (choice > 0)
+                        {
+                            switch (input1)
+                            {
+                                case "1":
+                                    Console.WriteLine("기사입니다");
+                                    break;
+                                case "2":
+                                    Console.WriteLine("궁수입니다");
+                                    break;
+                                case "3":
+                                    Console.WriteLine("전사입니다");
+                                    break;
+                            }
+
+                        }
+
+                        Console.WriteLine("기사입니다{0} 적의 체력{1}", 체력, 적체력);
+                        Console.WriteLine("1.공격합니다.");
+                        Console.WriteLine("2.방어합니다.");
+
+                        string input2 = Console.ReadLine();
+
+                        switch (input2)
+                        {
+                            case "1":
+                                Console.WriteLine("공격합니다");
+                                Console.WriteLine("남은 적의 체력");
+                                Console.WriteLine(적체력 -= (공격력 - (적방어력 / 2)));
+
+                                break;
+
+                            case "2":
+                                Console.WriteLine("방어합니다.");
+                                Console.WriteLine("남은 당신의 체력");
+                                Console.WriteLine(체력 -= (적공격력 - (방어력 / 2)));
+
+                                break;
+
+
+
+                        }
+
+
+
+
+                    }
+
+
+                }
+            }
+        
+
+    }
+
+}
