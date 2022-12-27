94551b71372b12833c4ae833692c7e2ac0560517
bobjon5556 <whanswkdz@gmail.com>
2022-12-27 17:50:53 +0900

    main

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,258 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int get_directionkey()//방향키를 입력받는 함수
+            {
+                int key;
+                key = _getch();
+                if (key == 224)//방향키
+                {
+                    return _getch(); //어떤 방향 키인지 반환
+                }
+                return 0;    //방향키가 아님
+            }
+            void print_puzzle(int puzzle[][3])
+{
+                int r, c;
+                system("cls"); //콘솔 화면을 지우기
+                for (r = 0; r < 3; r++)//열 반복
+                {
+                    for (c = 0; c < 3; c++)//행 반복
+                    {
+                        if (puzzle[r][c])//참이면(0이 아니면)
+                        {
+                            printf("%3d", puzzle[r][c]);//퍼즐의 수를 출력
+                        }
+                        else//거짓(0)이면
+                        {
+                            printf("   ");//공백 출력
+                        }
+
+                    }
+                    printf("\n");//개행
+                }
+            }
+            int is_ending(int puzzle[][3])
+{
+                int r, c;
+
+                for (r = 0; r < 3; r++)//열 반복
+                {
+                    for (c = 0; c < 3; c++)//행 반복
+                    {
+                        if (puzzle[r][c] != r * 3 + c + 1)
+                        {
+                            return (r == 2) && (c == 2);
+                        }
+                    }
+                }
+                return 0;
+            }
+            int main()
+            {
+                int puzzle[3][3] = { { 1,3,2 },{ 7,8,4 },{ 6,5,0 } };
+                int row = 2;
+                int col = 2;
+                int key = 0;
+
+                while (!is_ending(puzzle))
+                {
+                    print_puzzle(puzzle);
+                    printf(">> 방향키 선택 \n");
+                    key = get_directionkey();
+
+                    switch (key)
+                    {
+                        case RIGHT:
+                            if (col > 0)
+                            {
+                                puzzle[row][col] = puzzle[row][col - 1];
+                                puzzle[row][col - 1] = 0;
+                                col--;
+                            }
+
+                            break;
+                        case LEFT:
+                            if (col < 3 - 1)
+                            {
+                                puzzle[row][col] = puzzle[row][col + 1];
+                                puzzle[row][col + 1] = 0;
+                                col++;
+                            }
+                            break;
+                        case UP:
+                            if (row < 3 - 1)
+                            {
+                                puzzle[row][col] = puzzle[row + 1][col];
+                                puzzle[row + 1][col] = 0;
+                                row++;
+                            }
+                            break;
+                        case DOWN:
+                            if (row > 0)
+                            {
+                                puzzle[row][col] = puzzle[row - 1][col];
+                                puzzle[row - 1][col] = 0;
+                                row--;
+                            }
+                            break;
+                    }
+                }
+                return 0;
+            }
+
+            /* https://ehpub.co.kr
+       언제나 C언어 예제 Center
+       3X3 퍼즐 만들기
+    */
+
+# include
+# include
+# include
+# include
+
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
+#define SWAP(x,y) {int temp=x; x=y; y=temp;}
+
+            typedef struct Puzzle
+        {
+            char base[3][3];
+    int row;
+            int col;
+        }
+        Puzzle;
+
+void print_puzzle(Puzzle* puzzle)
+        {
+            int r, c;
+            system("cls");
+            for (r = 0; r < 3; r++)
+            {
+                for (c = 0; c < 3; c++)
+                {
+                    if (puzzle->base[r][c])
+            {
+                printf("%3d", puzzle->base[r][c]);
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int is_ending(Puzzle* puzzle)
+{
+    int r, c;
+
+    for (r = 0; r < 3; r++)
+    {
+        for (c = 0; c < 3; c++)
+        {
+            if (puzzle->base[r][c] == r * 3 + c + 1)
+            {
+    break;
+}
+        }
+    }
+    return (r == 2) && (c == 2);
+}
+
+void init_puzzle(Puzzle* puzzle)
+{
+    srand((unsigned)time(0));
+    int rvalues[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+    int rindex;
+    for (int i = 0; i < 8; i++)
+    {
+        while (1)
+        {
+            rindex = rand() % 8;
+            if (rvalues[rindex] != -1)
+            {
+                break;
+            }
+        }
+        puzzle->base[i / 3][i % 3] = rvalues[rindex];
+        rvalues[rindex] = -1;
+    }
+    puzzle->base[2][2] = 0;
+    puzzle->row = 2;
+    puzzle->col = 2;
+}
+int get_directionkey()
+{
+    int key = 0;
+    key = _getch();
+    if (key == 224)
+    {
+        return _getch();
+    }
+    return 0;
+}
+void move_puzzle(Puzzle* puzzle)
+{
+    int key;
+    printf(">> 방향키 선택\n");
+    key = get_directionkey();
+
+    char(*base)[3] = puzzle->base;
+    switch (key)
+    {
+        case RIGHT:
+            if (puzzle->col > 0)
+            {
+                SWAP(base[puzzle->row][puzzle->col], base[puzzle->row][puzzle->col - 1]);
+                puzzle->col--;
+            }
+            return;
+        case LEFT:
+            if (puzzle->col < 2)
+            {
+                SWAP(base[puzzle->row][puzzle->col], base[puzzle->row][puzzle->col + 1]);
+                puzzle->col++;
+            }
+            return;
+        case UP:
+            if (puzzle->row < 2)
+            {
+                SWAP(base[puzzle->row][puzzle->col], base[puzzle->row + 1][puzzle->col]);
+                puzzle->row++;
+            }
+            return;
+        case DOWN:
+            if (puzzle->row > 0)
+            {
+                SWAP(base[puzzle->row][puzzle->col], base[puzzle->row - 1][puzzle->col]);
+                puzzle->row--;
+            }
+            return;
+    }
+}
+int main()
+{
+    Puzzle puzzle;
+    Puzzle* ppuz = &puzzle;
+    init_puzzle(ppuz);
+    while (!is_ending(ppuz))
+    {
+        print_puzzle(ppuz);
+        move_puzzle(ppuz);
+    }
+    print_puzzle(ppuz);
+
+    system("pause");
+    return 0;
+}
+        }
+
+    }
+}
--- test1/test1/Program.cs
+++ test1/test1/Program.cs
@@ -1,52 +1,415 @@
-﻿using Microsoft.VisualBasic;
-using System;
-using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
-namespace baekjoon
+namespace Example
 {
-    class Program
+    // 퍼즐 박스
+    class Box
     {
-        static void Main(string[] args)
+        // 퍼즐 박스 크기
+        private const int BOX_SIZE = 50;
+        // 폰트 크기
+        private const int FONT_SIZE = 20;
+        // 폰트 설정
+        private Font FONT = new Font(new FontFamily("Arial"), FONT_SIZE, FontStyle.Bold, GraphicsUnit.Pixel);
+        // 색 설정
+        private Brush COLOR = new SolidBrush(Color.Black);
+        // 이미지 가져오기
+        private Image BOX = Bitmap.FromFile(AppDomain.CurrentDomain.BaseDirectory + "box.bmp");
+        // 공객 이미지 가져오기
+        private Image BLANK = Bitmap.FromFile(AppDomain.CurrentDomain.BaseDirectory + "blank.bmp");
+
+        // 퍼즐의 번호, 기본 마스크 설정
+        public Box(int number, Mask mask)
         {
-
-            Random rand = new Random();
-
-            int[] puzzle = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-
-
-            
-
-            int input = 0;
-
-            for (int i = puzzle.Length - 1; i > 0; i--)
+            // 퍼즐 번호 설정
+            this.Number = number;
+            // 마스크 설정
+            this.Mask = mask;
+        }
+        // 번호 취득
+        public int Number
+        {
+            get;
+            private set;
+        }
+        // 마스크 취득
+        public Mask Mask
+        {
+            get;
+            set;
+        }
+        // 그리기 (x 좌표, y 좌표)
+        public void Render(int x, int y)
+        {
+            // 0이면 공백 퍼즐임
+            if (Number == 0)
             {
-                int randomIndex = rand.Next(0, i + 1);
-
-                int temp = puzzle[i];
-                puzzle[i] = puzzle[randomIndex];
-                puzzle[randomIndex] = temp;
-
-                Console.WriteLine(temp);
+                // Bitmap 그리기
+                GDIBuffer.Graphics(g => g.DrawImage(BLANK, x, y, BOX_SIZE, BOX_SIZE));
+                return;
             }
-;
-
-            int[] puzzle2= { 8, 7, 5, 1, -1, 4, 6, 2, 3 };
-            int emptyTile = -1;
-            int numberInverions = 0;
-            for (int i = 0; i < puzzle2.Length - 1; i++)
-            {
-                if (puzzle[i] == emptyTile)
-                    continue;
-
-                for (int j = i + 1;  puzzle[j]) && (puzzle[j] != emptyTile))
-            numberInverions++;
+            // 글자의 위치 계산하기
+            float pointX = x + (BOX_SIZE / 2 - (FONT_SIZE / 2 + (int)Math.Log10(Number) * FONT_SIZE / 2)) + (Number > 9 ? 5 : 0);
+            float pointY = y + BOX_SIZE / 2 - FONT_SIZE / 2;
+            // Bitmap 그리기
+            GDIBuffer.Graphics(g => g.DrawImage(BOX, x, y, BOX_SIZE, BOX_SIZE));
+            // 번호 그리기
+            GDIBuffer.Graphics(g => g.DrawString(Number.ToString(), FONT, COLOR, new PointF(pointX, pointY)));
         }
     }
+    // 마스크 클래스(Box와 그릴 위치 좌표)
+    class Mask
+    {
+        // 마스크 생성
+        public Mask(int index, int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Index = index;
+            // Box 인스턴스 생성
+            this.Box = new Box(index, this);
+        }
+        // 퍼즐 박스
+        public Box Box
+        {
+            set;
+            get;
+        }
+        // X좌표 위치 설정
+        public int X
+        {
+            private set;
+            get;
+        }
+        // Y좌표 위치 설정
+        public int Y
+        {
+            private set;
+            get;
+        }
+        // 마스크 위치 설정
+        public int Index
+        {
+            private set;
+            get;
+        }
+    }
+    // 박스 테이블 (마스크 리스트를 상속)
+    class BoxTable : List<Mask>
+    {
+        // 박스는 좌로 5 세로 5의 총 25개 설정
+        private const int BOX_COUNT = 25;
+        // 공백 박스 클래스
+        private Box blankBox;
+        // 섞기 초기 플러그
+        public bool Init
+        {
+            get; set;
+        }
+        // 완료 플러그
+        public bool Complete
+        {
+            get; set;
+        }
+        // 생성장
+        public BoxTable()
+        {
+            //초기화 플러그 설정
+            Init = true;
+            // 완료 플러그 설정
+            Complete = false;
+            // 마스크 생성하기
+            CreateMask();
 
+        }
+        // 블랭크 박스 이동하기
+        public void MoveBlank(Keys key)
+        {
+            // 퍼즐이 맞춤이 완료되면 이동 금지
+            if (Complete)
+            {
+                return;
+            }
+            // Blank 위치 찾아오기
+            int pos = blankBox.Mask.Index;
+            // 이동할 위치
+            int mpos;
+            // 오른쪽 이동
+            if (key == Keys.Right)
+            {
+                // 오른쪽 끝이면 이동 못한다.
+                if (pos % 5 == 4)
+                {
+                    return;
+                }
+                // 이동
+                mpos = pos + 1;
+            }
+            // 왼쪽 이동
+            else if (key == Keys.Left)
+            {
+                // 왼쪽 끝이면 이동 못한다.
+                if (pos % 5 == 0)
+                {
+                    return;
+                }
+                // 이동
+                mpos = pos - 1;
+            }
+            // 아래로 이동
+            else if (key == Keys.Down)
+            {
+                // 아래쪽과 배열 차이는 5
+                mpos = pos + 5;
+            }
+            // 위로 이동
+            else if (key == Keys.Up)
+            {
+                // 위쪽과의 배열 차이는 5
+                mpos = pos - 5;
+            }
+            else
+            {
+                // 그 외의 버튼은 관계없음
+                return;
+            }
+            // 배열 범위를 넘어서면 리턴
+            if (mpos < 0 || mpos >= BOX_COUNT)
+            {
+                return;
+            }
+            // 마스크의 박스 객체 교환(Swap)
+            // 공백 박스가 있던 마스크에 이동할 박스를 넣는다.
+            blankBox.Mask.Box = base[mpos].Box;
+            // 기존의 이동한 박스에 공백 박스를 넣는다.
+            base[mpos].Box = blankBox;
 
+            // 각 박스의 마스크 재설정
+            base[pos].Box.Mask = base[pos];
+            base[mpos].Box.Mask = base[mpos];
 
-
-
+            // 랜덤 섞기가 끝난 상황이라면
+            if (!Init)
+            {
+                // 완료했는지 확인한다.
+                if (Check())
+                {
+                    // 완료 플로그 설정
+                    Complete = true;
+                    // 메시지 출력
+                    MessageBox.Show("Complete!!");
+                }
+            }
+        }
+        // 퍼즐 확인 함수
+        private bool Check()
+        {
+            // 각 마스크의
+            for (int i = 0; i < base.Count; i++)
+            {
+                // 번호가 순서대로 있으면
+                if (base[i].Box.Number == i)
+                {
+                    continue;
+                }
+                // 완료되지 않았다.
+                return false;
+            }
+            // 끝
+            return true;
+        }
+        // 마스크 생성
+        private void CreateMask()
+        {
+            // x좌표, y좌표 설정
+            int x = 0;
+            int y = 0;
+            // 총 25개
+            for (int i = 0; i < BOX_COUNT; i++)
+            {
+                // 좌로 5개가 꽉차면 개행
+                if (i % 5 == 0 && i != 0)
+                {
+                    x = 0;
+                    y += 50;
+                }
+                // 마스크를 리스트에 넣는다.
+                this.Add(new Mask(i, x, y));
+                // 넣을 때마다 50씩 증가
+                x += 50;
+            }
+            // 공객 박스는 맨 처음
+            blankBox = this[0].Box;
+        }
+        // 섞기 함수
+        public void Random()
+        {
+            // 0부터 4까지
+            switch (new Random().Next(4))
+            {
+                // 0 이면 왼쪽 이동
+                case 0: MoveBlank(Keys.Left); break;
+                // 1 이면 오른쪽 이동
+                case 1: MoveBlank(Keys.Right); break;
+                // 2 이면 위쪽 이동
+                case 2: MoveBlank(Keys.Up); break;
+                // 3 이면 아래쪽 이동
+                case 3: MoveBlank(Keys.Down); break;
+            }
+        }
+        // 그리기
+        public void Render()
+        {
+            // 마스크 취득
+            foreach (var m in this)
+            {
+                // 마스크에 있는 박스를 그리기
+                m.Box.Render(m.X, m.Y);
+            }
+        }
+    }
+    // 더블 버퍼링 클래스
+    class GDIBuffer
+    {
+        // 싱글톤
+        private static GDIBuffer instance = null;
+        // 그래픽 객체
+        private Graphics graphics;
+        // 이미지 객체
+        private Bitmap bitmap;
+        // 생성자
+        private GDIBuffer(Size size)
+        {
+            // 크기 별로 메모리 이미지 생성
+            this.bitmap = new Bitmap(size.Width, size.Height);
+            // 그래픽 객체 인스턴스 생성
+            this.graphics = System.Drawing.Graphics.FromImage(this.bitmap);
+        }
+        // 싱글톤 초기화
+        public static void Init(Size size)
+        {
+            // 기존 인스턴스가 있으면
+            if (instance != null)
+            {
+                // 메모리 해지하고 재 생성한다.
+                instance.graphics.Dispose();
+                instance.bitmap.Dispose();
+            }
+            // 인스턴스 생성
+            instance = new GDIBuffer(size);
+        }
+        // Graphic 취득 함수
+        public static void Graphics(Action<Graphics> func)
+        {
+            // 싱글톤이 생성되 있지 않으면 return;
+            if (instance == null)
+            {
+                return;
+            }
+            // lock을 걸고
+            lock (instance.graphics)
+            {
+                // Graphic 객체를 넘긴다.
+                func(instance.graphics);
+            }
+        }
+        // 버퍼를 Graphic 객체로 그린다.
+        public static void Render(Graphics g)
+        {
+            // 싱글톤이 생성되 있지 않으면 return;
+            if (instance == null)
+            {
+                return;
+            }
+            // Graphic에 버퍼를 그린다.
+            g.DrawImage(instance.bitmap, new Point(0, 0));
+        }
+    }
+    // 폼 클래스
+    class Program : Form
+    {
+        // 박스 테이블
+        private BoxTable boxtable;
+        // 생성자
+        public Program()
+        {
+            // 윈도우 폼 설정 (크기 조절 안되는 창)
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            // 윈도우 사이즈 설정
+            this.Size = new Size(265, 290);
+            // 버퍼 초기화(윈도우 사이즈로 맞춘다.)
+            GDIBuffer.Init(new Size(ClientRectangle.Width, ClientRectangle.Height));
+            // 박스 테이블 인스턴스 생성
+            boxtable = new BoxTable();
+            // 폼의 키 이벤트
+            this.KeyDown += (s, e) =>
+            {
+                // Black 퍼즐을 이동한다.
+                boxtable.MoveBlank(e.KeyCode);
+            };
+            // 쓰레드 생성
+            ThreadPool.QueueUserWorkItem((c) =>
+            {
+                // 무한 루프로 버퍼 및 화면에 그린다.
+                while (true)
+                {
+                    // 그리기..
+                    Render();
+                    // 1초에 24 프레임으로
+                    Thread.Sleep(1000 / 24);
+                }
+            });
+            // 섞기 쓰레드 생성
+            ThreadPool.QueueUserWorkItem((c) =>
+            {
+                // 게임 시작 메시지
+                MessageBox.Show("Game start..");
+                // 랜덤식으로 500번 섞기
+                for (int i = 0; i < 500; i++)
+                {
+                    // 위치로 랜덤식으로 이동
+                    boxtable.Random();
+                    // 0.01 단위로
+                    Thread.Sleep(10);
+                }
+                // 초기화 끝
+                boxtable.Init = false;
+            });
+        }
+        // 그리기 함수
+        public void Render()
+        {
+            // 버퍼 프레임 업데이트
+            FrameUpdate();
+            // 화면에 버퍼 프레임을 그리기
+            using (var g = CreateGraphics())
+            {
+                GDIBuffer.Render(g);
+            }
+        }
+        // 버퍼 프레임 업데이트 함수
+        private void FrameUpdate()
+        {
+            // 배경을 흰색으로 클리어
+            GDIBuffer.Graphics(g => g.Clear(Color.White));
+            // 퍼즐 box 그리기
+            boxtable.Render();
+        }
+        // 싱글 스레드 어트리뷰트
+        [STAThread]
+        // 실행 함수	
+        static void Main(string[] args)
+        {
+            // 환경에 맞는 스타일 설정	
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // 메시지 루프에 인스턴스를 생성한다.
+            Application.Run(new Program());
+        }
+    }
 }
-    
